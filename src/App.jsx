1db61e8ff240a23cb803a09307b933a3798128d7
Ronald Santamaria <pizarroronald06@gmail.com>
2023-11-05 19:20:14 -0300

    Landing React - Nucba

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Hero from "./components/Hero/Hero";
 import Products from "./components/Products/Products";
 import About from "./components/About/About";
 import Footer from "./components/Footer/Footer";
-import { Main } from "./styles/main";
+import { Main } from "./styles/Main";
 
 function App() {
   return (
